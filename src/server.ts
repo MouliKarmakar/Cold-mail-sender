8971324b2ac719248228983671198d8ad0ba3598
Mouli <moulikarmakar7596@gmail.com>
2026-04-16 21:06:11 +0530

    Now accessing port from env

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from './app';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const PORT=8000;
+const PORT=process.env.PORT || 8000;
 const Server=http.createServer(app);
 Server.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`);
